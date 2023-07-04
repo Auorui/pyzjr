06a0209290cbe83efbb38ad780ace02b954dd22e
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2023-07-04 11:24:54 +0800

    Update __init__.py

--- pyzjr/__init__.py
+++ pyzjr/__init__.py
@@ -15,24 +15,26 @@ from pyzjr import pysift as sift  # Using SIFT matching algorithm
 from pyzjr import Color as color
 """
 
-
-__version__ = "0.0.1"
+__version__ = "0.0.7"
 
 import cv2
-
-from video import VideoCap,FPS,Mp4toGif
-from utils import stackImages,addnoisy,getContours,empty
+from pyzjr.PIC import download_file,getPhotopath,Pic_rename,read_resize_image,\
+                      load_images_from_folder,save_images
+from pyzjr.Enimage import Filter,Enhance,Random_Enhance,Retinex,\
+                          repair_Img
+from pyzjr.definition import Fuzzy_image,Clear_quantification
+from pyzjr.ColorModule import *
+from pyzjr.definition import *
+from pyzjr.TrackBar import *
+from pyzjr.utils import *
+from pyzjr.video import *
+from pyzjr.Z import * 
 
 repair_TELEA=cv2.INPAINT_TELEA
 repair_NS=cv2.INPAINT_NS
 Lap_64F=cv2.CV_64F
 
-
-
-
-
-
-
+print("test successful")
 
 
 
