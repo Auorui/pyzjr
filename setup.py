ee5ca0d245076a1df2fb0d9d48cf60883371c973
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-02-07 11:17:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 - pyzjr is the Codebase accumulated by my python programming.
   At present, it is only for my personal use. If you want to
   use it, please contact me. Here are my email and WeChat.
-- WeChat: z15583909992
+- WeChat: zjricetea
 - Email: zjricetea@gmail.com
 - Note: Currently still being updated, please refer to the latest version for any changes that may occur
 
