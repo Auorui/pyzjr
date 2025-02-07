6ef2abbe3502341e64b1b49107ff9f4af403abf4
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-02-07 11:16:19 +0800

    Delete nn directory

--- nn/models/atten/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .se_module import SqueezeExcitation, SEAttention, EffectiveSEAttention
-from .bam_module import BAMAttention
-from .cbam_module import CBAMAttention
-from .cot_module import CoTAttention
